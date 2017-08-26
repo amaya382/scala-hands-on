ea4ba1f5ce8284a2563f146ec59ba6bddb0f1915
amaya <mail@sapphire.in.net>
2017-08-26 12:07:16 +0900

    Add `parseIntArrayJSON`

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,26 @@
 import sbt.io.IO
 import java.io.File
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
 
 object Main {
   def main(args: Array[String]): Unit = {
     val str = readFile("sample.json")
     println(str)
+
+    val ints = parseIntArrayJSON(str)
+    println(ints)
   }
 
   def readFile(fileName: String): String =
     IO.read(new File(fileName))
+
+  def parseIntArrayJSON(jsonString: String): Seq[Int] = {
+    val json = parse(jsonString)
+    for {
+      JArray(array) <- json
+      JInt(x) <- array
+    } yield x.toInt
+  }
 }
