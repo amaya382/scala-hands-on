747ac40af0a6d22ed0d0571d5b5df418b2986c1f
amaya <mail@sapphire.in.net>
2017-08-26 12:02:04 +0900

    Add `readFile`

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,13 @@
+import sbt.io.IO
+import java.io.File
+
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    val str = readFile("sample.json")
+    println(str)
   }
+
+  def readFile(fileName: String): String =
+    IO.read(new File(fileName))
 }
