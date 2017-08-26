7db4e4d829eca59b34bcdc5b89dbb1b4aceeedde
amaya <mail@sapphire.in.net>
2017-08-26 12:29:50 +0900

    Add test

--- src/test/scala/MainSpec.scala
+++ src/test/scala/MainSpec.scala
@@ -1,4 +1,27 @@
+import java.io.File
+import org.json4s._
+import org.json4s.jackson.JsonMethods
+import org.scalatest._
+import sbt.io.IO
 
-class MainSpec {
 
+class MainSpec extends FlatSpec {
+  "FzzBuzzed sample.json" should "have the same elements as answer.json" in {
+    val sampleString = IO.read(new File("sample.json"))
+    val sampleJSON = JsonMethods.parse(sampleString)
+    val sample = for {
+      JArray(arr) <- sampleJSON
+      JInt(x) <- arr
+    } yield x.toInt
+    val fizzBuzzedSample = Main.toFizzBuzz(sample)
+
+    val answerString = IO.read(new File("answer.json"))
+    val answerJSON = JsonMethods.parse(answerString)
+    val answer = for {
+      JArray(arr) <- answerJSON
+      JString(x) <- arr
+    } yield x
+
+    assert(fizzBuzzedSample.sameElements(answer))
+  }
 }
